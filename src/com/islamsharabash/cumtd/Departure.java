4141709ec71ffabcb1dd291e7a8ad8410f996bdc
Islam Sharabash <islam.sharabash@gmail.com>
2012-07-01 15:00:50 -0700

    use rectangle on left for color

--- src/com/islamsharabash/cumtd/Departure.java
+++ src/com/islamsharabash/cumtd/Departure.java
@@ -1,78 +1,60 @@
 package com.islamsharabash.cumtd;
 
 import android.graphics.Color;
-import android.text.Html;
-import android.text.Spanned;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-//TODO this will depend on the new version of the api (to be released within a week of 11/15)
-// TODO hold out for this: http://developer.cumtd.com/forum/default.aspx?g=posts&m=89&#post89
 public class Departure {
-	
-	private String route;
-	private int minutes;
-	private String color;
-	private float  LIGHT_OFFSET= .4f;
-	private float SAT_OFFSET = -.15f;
-	
-	public Departure(String route, int minutes, String color) {
-		this.route = route;
-		this.minutes = minutes;
-		this.color = brightenColor("#" + color);
-	}
-	
-	// returns string hex of brightened color
-	private String brightenColor(String str_color) {
-		int color = Color.parseColor(str_color);
-		float[] hsv = new float[3];
-		Color.colorToHSV(color, hsv);
-		
-		hsv[2] = hsv[2] + LIGHT_OFFSET;
-		hsv[1] = hsv[1] + SAT_OFFSET;
-		
-		int new_color = Color.HSVToColor(hsv);
-		
-		return String.format("#%06X", (0xFFFFFF & new_color));
-	}
+  private String route;
+  private int minutes;
+  private int color;
+  private float  LIGHT_OFFSET= .2f;
+  private float SAT_OFFSET = -.12f;
 
-	// returns string for route name
-	// using html to get perty colors :)
-	public Spanned getRoute() {
-		int index = this.route.indexOf(' ');
-		String code = this.route.substring(0, index);
-		String name = this.route.substring(index, this.route.length());
-		
-		return Html.fromHtml(
-				"<font color=#FFFFFF>" + code + "</font>" +
-				"<font color=" + this.color + ">" + name + "</font>");
-	}
-	
-	// returns text to display for expected time
-	public String getTime() {
-		if (this.minutes == 0) {
-			return "DUE";
-		}
-		
-        /*TODO I used h (12 hour) instead of H (24 hour). I had difficulty trying to get access to the user's preference.
-        If someone puts in a toggle for this, they should ask if they want 12 or 24 hour time, and change this to
-        "H:mm" if the user prefers 24 hour time.*/ 
-        SimpleDateFormat timingFormat = new SimpleDateFormat("h:mm");
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MINUTE, this.minutes);
+  public Departure(String route, int minutes, String color) {
+    this.route = route;
+    this.minutes = minutes;
+    this.color = brightenColor("#" + color);
+  }
 
-		return Integer.toString(this.minutes) + "m " + timingFormat.format(cal.getTime());
-	}
-	
-	// returns the color 
-	public int getTimeColor() {
-		if (this.minutes == 0) {
-			return Color.RED;
-		}
-		
-		return Color.WHITE;
-	}
-	
+  public int getColor() {
+    return this.color;
+  }
 
+  public String getRoute() {
+    return this.route;
+  }
+
+  public String getTime() {
+    if (this.minutes == 0) {
+      return "DUE";
+    }
+
+    /*TODO I used h (12 hour) instead of H (24 hour). I had difficulty trying to get access to the user's preference.
+      If someone puts in a toggle for this, they should ask if they want 12 or 24 hour time, and change this to
+      "H:mm" if the user prefers 24 hour time.*/ 
+    SimpleDateFormat timingFormat = new SimpleDateFormat("h:mm");
+    Calendar cal = Calendar.getInstance();
+    cal.add(Calendar.MINUTE, this.minutes);
+
+    return Integer.toString(this.minutes) + "m " + timingFormat.format(cal.getTime());
+  }
+
+  public int getTimeColor() {
+    if (this.minutes == 0) {
+      return Color.RED;
+    }
+    return Color.WHITE;
+  }
+
+  private int brightenColor(String str_color) {
+    int color = Color.parseColor(str_color);
+    float[] hsv = new float[3];
+    Color.colorToHSV(color, hsv);
+
+    hsv[2] = hsv[2] + LIGHT_OFFSET;
+    hsv[1] = hsv[1] + SAT_OFFSET;
+
+    return Color.HSVToColor(hsv);
+  }
 }
--- src/com/islamsharabash/cumtd/DepartureAdapter.java
+++ src/com/islamsharabash/cumtd/DepartureAdapter.java
@@ -4,70 +4,79 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
-import android.view.*;
-import android.widget.*;
-
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 // similar to the ArrayAdapter, but doesn't need a text view
 public class DepartureAdapter extends BaseAdapter implements ListAdapter {
-	
-	private List<Departure> departures = new ArrayList<Departure>();
-	private LayoutInflater inflater;
-	
-	public DepartureAdapter(Context context) {
-		this.inflater = LayoutInflater.from(context);
-	}
-	
-	public void setDepartures(List<Departure> departures) {
-		this.departures = departures;
-	}
 
-	@Override
-	public int getCount() {
-		return this.departures.size();
-	}
+  private List<Departure> departures = new ArrayList<Departure>();
+  private LayoutInflater inflater;
 
-	@Override
-	public Object getItem(int position) {
-		return this.departures.get(position);
-	}
+  public DepartureAdapter(Context context) {
+    this.inflater = LayoutInflater.from(context);
+  }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+  public void setDepartures(List<Departure> departures) {
+    this.departures = departures;
+  }
 
-	// ref: http://developer.android.com/resources/samples/ApiDemos/src/com/example/android/apis/view/List14.html
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		
-		// create the view or get references to view components
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.departure_list_item, null);	
-			
-            holder = new ViewHolder();
-            holder.route = (TextView) convertView.findViewById(R.id.route);
-            holder.time = (TextView) convertView.findViewById(R.id.time);
-            
-            convertView.setTag(holder);
-            
-		} else {
-	        holder = (ViewHolder) convertView.getTag();
-		}
-		
-		// set data
-		Departure departure = this.departures.get(position);
-		holder.route.setText(departure.getRoute());
-		holder.time.setText(departure.getTime());
-		
-		holder.time.setTextColor(departure.getTimeColor());
-		
-		return convertView;
-	}
-	
-	static class ViewHolder {
-	    TextView route;
-	    TextView time;
-	}
+  @Override
+    public int getCount() {
+      return this.departures.size();
+    }
+
+  @Override
+    public Object getItem(int position) {
+      return this.departures.get(position);
+    }
+
+  @Override
+    public long getItemId(int position) {
+      return position;
+    }
+
+  // ref: http://developer.android.com/resources/samples/ApiDemos/src/com/example/android/apis/view/List14.html
+  @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      ViewHolder holder;
+
+      // create the view or get references to view components
+      if (convertView == null) {
+        convertView = inflater.inflate(R.layout.departure_list_item, parent, false);  
+        holder = new ViewHolder();
+
+        View stop_color_view = (View) convertView.findViewById(R.id.stop_color_shape);
+        holder.stopColor = (GradientDrawable) stop_color_view.getBackground();
+        holder.route = (TextView) convertView.findViewById(R.id.route);
+        holder.time = (TextView) convertView.findViewById(R.id.time);
+
+        convertView.setTag(holder);
+
+      } else {
+        holder = (ViewHolder) convertView.getTag();
+      }
+
+      // set data
+      Departure departure = this.departures.get(position);
+      holder.stopColor.setColor(departure.getColor());
+      holder.route.setText(departure.getRoute());
+      holder.time.setText(departure.getTime());
+
+      holder.time.setTextColor(departure.getTimeColor());
+
+      return convertView;
+    }
+
+  static class ViewHolder {
+    GradientDrawable stopColor;
+    TextView route;
+    TextView time;
+  }
 }
